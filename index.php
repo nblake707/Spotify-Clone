565d594f02f936906855bb9ed39004083bf97867
nblake707 <nblake707@gmail.com>
2019-04-25 14:29:34 -0400

    began adding php for registration page

--- index.php
+++ index.php
@@ -2,22 +2,27 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Spotify Clone</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-       
-       Hello!
-        
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Spotify Clone</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+    Hello!
 
 
 
-        <script src="" async defer></script>
-    </body>
+
+    <script src="" async defer></script>
+</body>
+
 </html>
--- register.php
+++ register.php
@@ -1,39 +1,93 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Welcome to Spotify!</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-     
-     <div id="inputContainer">
+<?php
+//if login button has been pressed do something
+if(isset($_POST['loginButton'])) {
+    //login button was pressed
 
-    <!-- action portion determines page where data will be sent -->
-     <form id="login" action="register.php" method="post">
-     <h2>Login to your account </h2>
-     <p>
-     <label for="loginuserName">Username</label>
-     <input id="loginUsername" name="loginUserName" type="text" placeholder="e.g. dDraper" required> 
-     </p>
-    <p>
-    <label for="loginPassword">Password</label>
-    <input id="loginPassword" name="loginPassword" type="password" placeholder="e.g. dDraper" required> 
-    </p>
-     
-     <button type="submit" name="loginButton">LOG IN</button>
-     </form>
-        
-     </div>
+}
 
-    
-        
-        <script src="" async defer></script>
-    </body>
+if(isset($_POST['registerButton'])) {
+    //register button was pressed
+
+    //created a variable called username. Saved input from username portion of form within this variable. 
+   $username = $_POST['username'];
+   echo $username;
+}
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Welcome to Spotify!</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+<body>
+
+    <div id="inputContainer">
+
+        <!-- action portion determines page where data will be sent -->
+        <form id="loginForm" action="register.php" method="post">
+            <h2>Login to your account </h2>
+            <p>
+                <label for="loginuserName">Username</label>
+                <input id="loginUsername" name="loginUserName" type="text" placeholder="e.g. dDraper" required>
+            </p>
+            <p>
+                <label for="loginPassword">Password</label>
+                <input id="loginPassword" name="loginPassword" type="password" placeholder="e.g. dDraper" required>
+            </p>
+
+            <button type="submit" name="loginButton">LOG IN</button>
+
+        </form>
+
+        <form id="registerForm" action="register.php" method="post">
+
+            <h2>Register for a free account</h2>
+            <p>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" placeholder="e.g. pOlsen" required>
+            </p>
+
+            <p>
+                <label for="firstName">First Name</label>
+                <input id="firstName" name="firstName" type="text" placeholder="e.g. Peggy" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input id="lastName" name="lastName" type="text" placeholder="e.g. Olsen" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="" required>
+            </p>
+            <p>
+                <label for="confirmEmail">Confirm Email</label>
+                <input id="confirmEmail" name="confirmEmail2" type="email" placeholder="" required>
+            </p>
+
+
+            <p>
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" placeholder="Your Password" required>
+            </p>
+            <p>
+                <label for="password2">Confirm Password</label>
+                <input id="password2" name="password2" type="password" placeholder="Conform Your Password" required>
+            </p>
+
+            <button type="submit" name="registerButton">Sign Up</button>
+        </form>
+
+
+    </div>
+
+
+
+    <script src="" async defer></script>
+</body>
+
 </html>
