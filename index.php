f445f86996ec8af6b66c7844ea1add7b7a1de171
nblake707 <nblake707@gmail.com>
2019-04-25 00:42:49 -0400

    created basic structure of the login page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-html-5 <!DOCTYPE html>
+<!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
@@ -12,10 +12,12 @@ html-5 <!DOCTYPE html>
         <link rel="stylesheet" href="">
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
+       
+       Hello!
         
+
+
+
         <script src="" async defer></script>
     </body>
 </html>
---
+++ register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Welcome to Spotify!</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+     
+     <div id="inputContainer">
+
+    <!-- action portion determines page where data will be sent -->
+     <form id="login" action="register.php" method="post">
+     <h2>Login to your account </h2>
+     <p>
+     <label for="loginuserName">Username</label>
+     <input id="loginUsername" name="loginUserName" type="text" placeholder="e.g. dDraper" required> 
+     </p>
+    <p>
+    <label for="loginPassword">Password</label>
+    <input id="loginPassword" name="loginPassword" type="password" placeholder="e.g. dDraper" required> 
+    </p>
+     
+     <button type="submit" name="loginButton">LOG IN</button>
+     </form>
+        
+     </div>
+
+    
+        
+        <script src="" async defer></script>
+    </body>
+</html>
